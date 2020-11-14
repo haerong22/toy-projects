7112f17107142ae6b34aab87e8e857f498a27d02
haerong22 <haerong22@naver.com>
2020-11-14 16:30:04 +0900

    - 비디오 업로드 - 썸네일 생성 및 저장

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,6 +1,7 @@
 import React,{useState} from 'react';
 import { Typography, Button, Form, message, Input, Icon } from 'antd';
 import Dropzone from 'react-dropzone';
+import Axios from 'axios';
 
 const { TextArea } = Input;
 const { Title } = Typography;
@@ -21,6 +22,9 @@ function VideoUploadPage() {
     const [Description, setDescription] = useState("")
     const [Private, seTPrivate] = useState(0)
     const [Category, setCategory] = useState("Film & Animation")
+    const [FilePath, setFilePath] = useState("")
+    const [Duration, setDuration] = useState("")
+    const [ThumbnailPath, setThumbnailPath] = useState("")
 
     const onTitleChange = (e) => {
         setVideoTitle(e.currentTarget.value)
@@ -34,6 +38,41 @@ function VideoUploadPage() {
     const onCategoryChange = (e) => {
         setCategory(e.currentTarget.value)
     }
+    const onDrop = (files) => {
+        let formData = new FormData;
+        const config = {
+            header: {'content-type': 'multipart/form-data'}
+        }
+        formData.append("file", files[0])
+
+        Axios.post('/api/video/uploadfiles', formData, config)
+            .then(response => {
+                if(response.data.success) {
+                    console.log(response.data)
+                    
+                    let variable = {
+                        url: response.data.url,
+                        fileName: response.data.fileName
+                    }
+                    
+                    setFilePath(response.data.url)
+
+                    Axios.post('/api/video/thumbnail', variable)
+                    .then(response => {
+                        if(response.data.success) {
+                            console.log(response.data)
+                            setDuration(response.data.fileDuration)
+                            setThumbnailPath(response.data.url)
+
+                        } else {
+                            alert('썸네일 생성에 실패 했습니다.')
+                        }
+                    })
+                } else {
+                    alert('비디오 업로드를 실패했습니다.')
+                }
+            })
+    }
 
     return (
         <div style={{ maxWidth:'700px', margin:'2rem auto' }}>
@@ -44,9 +83,9 @@ function VideoUploadPage() {
                 <div style={{ display:'flex', justifyContent:'space-between' }}>
                     {/* Drop zone */}
                     <Dropzone 
-                    onDrop
-                    multiple
-                    maxSize
+                    onDrop={onDrop}
+                    multiple={false}
+                    maxSize={100000000}
                     >
                     {({ getRootProps, getInputProps }) => (
                         <div style={{ width:'300px', height:'240px', border:'1px solid lightgray', display:'flex',
@@ -57,9 +96,11 @@ function VideoUploadPage() {
                     )}
                     </Dropzone>
                     {/* Thumbnail  */}
-                    <div>
-                        <img src alt />
-                    </div>
+                    {ThumbnailPath &&
+                        <div>
+                            <img src={`http://localhost:5000/${ThumbnailPath}`} alt="thumbnail" />
+                        </div>
+                    }
                 </div>
         <br />
         <br />
